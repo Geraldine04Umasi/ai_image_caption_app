3cb170e6d06f22dff21c58a52b25c26a8fe178d0
Geraldine Umasi <gumasi@unsa.edu.pe>
2026-03-19 19:40:35 -0500

    feat(storage): implement file upload functionality with AWS S3
    - Added AWS SDK S3 client configuration in aws.config.ts
    - Integrated dotenv for environment variable management
    - Created StorageController to handle file uploads
    - Implemented uploadFile method in StorageService to upload files to S3
    - Added unit tests for StorageController
    - Updated package.json to include necessary dependencies (aws-sdk, dotenv, multer)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  app.enableCors();
+
+  await app.listen(3001);
 }
 bootstrap();
--- backend/src/modules/storage/storage.service.ts
+++ backend/src/modules/storage/storage.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { s3 } from '../../config/aws.config';
 
 @Injectable()
-export class StorageService {}
+export class StorageService {
+  async uploadFile(file: Express.Multer.File) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: fileName,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    });
+
+    await s3.send(command);
+
+    console.log('REGION:', process.env.AWS_REGION);
+    console.log('BUCKET:', process.env.AWS_BUCKET_NAME);
+
+
+    return {
+      fileName,
+      url: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`,
+    };
+  }
+}
